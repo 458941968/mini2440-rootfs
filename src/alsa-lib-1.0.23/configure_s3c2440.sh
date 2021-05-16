0293dd381832f2bf508fb820120301fe0e05b6a2
hyq <458941968@qq.com>
2021-05-16 11:26:28 +0800

    mv rootfs from src to bin

---
+++ src/alsa-lib-1.0.23/configure_s3c2440.sh
@@ -0,0 +1,1 @@
+./configure --host=arm-linux --prefix=/user/share/alsa  --disable-python
---
+++ src/alsa-utils-1.0.23/configure_s3c2440.sh
@@ -0,0 +1,1 @@
+./configure --host=arm-linux --prefix=/user/share/alsa CFLAGS="-I/user/share/alsa/include" LDFLAGS="-L/user/share/alsa/lib -lasound "  --disable-alsamixer --disable-xmlto
